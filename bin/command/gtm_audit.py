eb07c57db4d9781981f65b8a4f0dd8f2dff7f567
Peak Wongharoen <pwongcha@akamai.com>
2023-12-13 15:58:24 -0600

    refactor: gtm command

--- bin/command/gtm_audit.py
+++ bin/command/gtm_audit.py
@@ -4,8 +4,11 @@ import csv
 import ipaddress
 import sys
 
+import numpy as np
 import pandas as pd
+import pydig
 from akamai_api.gtm import GtmWrapper
+from pandarallel import pandarallel
 from rich import print_json
 from utils import files
 
@@ -17,13 +20,31 @@ def get_nickname(x):
         return 0  #
 
 
-def validate_ip(x):
-    if pd.isna(x) or x == '0':
-        return None
+def validate_ip(value):
+    if pd.isna(value) or value == '0':
+        return False
     try:
-        return ipaddress.ip_address(x)
+        new_value = ipaddress.ip_address(value)
+        if value == new_value:
+            return True
     except ValueError:
-        return None
+        x = pydig.query(value, 'A')
+        new_value = ''
+        try:
+            if isinstance(x, list) and len(x) == 0:
+                return False
+            elif isinstance(x, list) and len(x) > 0:
+                new_value = ipaddress.ip_address(x[0])
+                if x[0] == new_value:
+                    return True
+            else:
+                new_value = ipaddress.ip_address(x)
+                if x == new_value:
+                    return True
+        except ValueError:
+            if x[0] == new_value:
+                return True
+        return False
 
 
 def audit(args, account_folder, logger):
@@ -52,13 +73,12 @@ def audit(args, account_folder, logger):
         for ind, p in enumerate(property):
             prop_dict = property[ind]['trafficTargets']
             property_name = property[ind]['name']
+
             df = pd.DataFrame(prop_dict)
             df['Domain'] = domain
             df['Properties'] = property_name
-
-            df['get_datacenter'] = df.apply(lambda row: gtm.get_datacenter(row['Domain'], row['datacenterId']), axis=1)
-            df['get_datacenter'] = df['get_datacenter'].apply(get_nickname)
-
+            # df = df[df['enabled']].copy()
+            # df = df[df['weight'] > 0].copy()
             try:
                 df['Type'] = property[ind]['type']
             except:
@@ -68,17 +88,26 @@ def audit(args, account_folder, logger):
             combine_df.append(df)
 
     master_df = pd.concat(combine_df)
-    master_df['valid_ip'] = master_df['servers'].apply(validate_ip)
-    sort_col_df = master_df[['Domain', 'Properties', 'Type', 'enabled',
-                             'weight', 'servers', 'name', 'handoutCName', 'valid_ip'
-                             ]]
-    master_df = sort_col_df.sort_values(by=['Domain', 'Properties', 'weight', 'enabled'],
-                                        ascending=[True, True, False, False],
+    cols = ['Domain', 'Properties', 'servers', 'handoutCName']
+
+    master_df = master_df[master_df['enabled']].copy()
+    master_df = master_df[master_df['weight'] > 0].copy()
+    master_df['servers'] = master_df['servers'].fillna(master_df['handoutCName'])
+
+    pandarallel.initialize(progress_bar=False, nb_workers=5, verbose=0)
+    master_df['datacenter'] = master_df.parallel_apply(lambda row: gtm.get_datacenter(row['Domain'], row['datacenterId']), axis=1)
+    master_df['datacenter'] = master_df['datacenter'].parallel_apply(get_nickname)
+    master_df['valid_ip'] = master_df['servers'].parallel_apply(validate_ip)
+    master_df['gtm'] = master_df['Properties'] + '.' + master_df['Domain']
+
+    sort_col_df = master_df[['gtm', 'Domain', 'Properties', 'Type', 'datacenter', 'weight', 'servers']]
+    master_df = sort_col_df.sort_values(by=['Domain', 'Properties', 'weight'],
+                                        ascending=[True, True, False],
                                         )
 
     # cumulative counts of group by object
-    cc = master_df.groupby(['Domain', 'Properties', 'Type']).cumcount() + 1
-    flat_df = master_df.set_index(['Domain', 'Properties', 'Type', cc]
+    cc = master_df.groupby(['gtm', 'Domain', 'Properties', 'Type']).cumcount() + 1
+    flat_df = master_df.set_index(['gtm', 'Domain', 'Properties', 'Type', cc]
                                   ).unstack().sort_index(axis='columns', level=1)
     flat_df.columns = ['_'.join(map(str, i)) for i in flat_df.columns]
     flat_df.reset_index(inplace=True)
@@ -86,8 +115,11 @@ def audit(args, account_folder, logger):
     sheet['flat_table'] = flat_df
     sheet['raw_data'] = master_df
 
+    group_df = master_df[['gtm', 'datacenter', 'servers']]
+    grouped = group_df.groupby('gtm').agg(lambda x: x.tolist()).reset_index()
+    sheet['summary'] = grouped
     if sheet:
-        files.write_xlsx(filepath, sheet)
+        files.write_xlsx(filepath, sheet, freeze_column=3)
         files.open_excel_application(filepath, not args.no_show, flat_df)
 
 
